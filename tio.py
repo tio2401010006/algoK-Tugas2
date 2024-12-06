6c92f02e53b6ad9dff7940cd36c9a413683eaec5
tio2401010006 <tyoade74@gmail.com>
2024-12-06 11:57:25 +0700

    commit pertama

---
+++ tio.py
@@ -0,0 +1,11 @@
+#Program konversi satuan meter ke centimeter
+print("Konversi satuan meter ke dalam centimeter")
+#INPUT nilai dalam meter
+m = float (input("Masukan nilai dalam meter : "))
+
+#Proses Hitung konversi
+cm = m * 100
+hsl = ("Panjang dalam cm adalah : " +str(cm)+ "cm")
+
+#Cetak Hasil 
+print(hsl)
